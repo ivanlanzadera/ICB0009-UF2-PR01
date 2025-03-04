15c7664e53da7785ff233d14adf5cacacc410d05
Iván Lanzadera <ivanlanzadera@gmail.com>
2025-03-04 14:37:02 +0100

    Cumplimiento Ejercicio 1 - Tarea 2

--- Ejercicio1/Tarea2/Program.cs
+++ Ejercicio1/Tarea2/Program.cs
@@ -1,12 +1,90 @@
 ﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Concurrent;
 
 namespace _Tarea2
 {
     class Program
-    {
+    {   
+        static SemaphoreSlim SemMedicos = new SemaphoreSlim(4);
+        static ConcurrentQueue<Paciente> ColaPacientes = new ConcurrentQueue<Paciente>();
+        static BlockingCollection<Paciente> Intercambio = new BlockingCollection<Paciente>(ColaPacientes);
+        static Stopwatch stopwatch = Stopwatch.StartNew();
+        static int Llegadas = 1;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola");
+            Thread T1 = new Thread(Consulta);
+            Thread T2 = new Thread(Consulta);
+            Thread T3 = new Thread(Consulta);
+            Thread T4 = new Thread(Consulta);
+
+            T1.Start();
+            Thread.Sleep(2000);
+            T2.Start();
+            Thread.Sleep(2000);
+            T3.Start();
+            Thread.Sleep(2000);
+            T4.Start();
+
+            Console.ReadLine();
+        }
+
+        static void Consulta ()
+        {
+            SemMedicos.Wait();
+            Console.WriteLine("Ha entrado el {0} paciente", Llegadas);
+            int tiempoLlegada = (int)stopwatch.Elapsed.TotalSeconds;
+            Paciente paciente = new Paciente(IdAleatorio(), tiempoLlegada, numAleatorio(), Llegadas++, 1);
+            Intercambio.Add(paciente);
+            Thread.Sleep(paciente.TiempoConsulta*1000);
+            SemMedicos.Release();
+            Console.WriteLine("Ha salido el paciente {0}: tiempo llegada: {1}s, tiempo consulta: {2}s, orden de llegada: {3}, prioridad: N{4}",
+             paciente.Id, paciente.LlegadaHospital, paciente.TiempoConsulta, paciente.NumeroLlegada, paciente.Prioridad);
+        }
+
+        private static int numAleatorio()
+        {
+            Random rnd = new Random();
+            return rnd.Next(5,16);
+        }
+
+        private static int IdAleatorio()
+        {
+            Random rnd = new Random();
+            bool disponible = false;
+            int id = 0;
+            while(!disponible)
+            {
+                disponible = true;
+                id = rnd.Next(1, 101);
+                Parallel.ForEach (Intercambio, paciente => {
+                    if (paciente.Id == id) disponible = false;
+                });
+            }
+            return id;
+        }
+    
+    }
+
+    public class Paciente
+    {
+        public int Id {get;set;}
+        public int LlegadaHospital {get;set;}
+        public int TiempoConsulta {get;set;}
+        public int Estado {get;set;}
+        public int NumeroLlegada {get;set;}
+        public int Prioridad {get;set;}
+
+        public Paciente (int Id, int LlegadaHospital, int TiempoConsulta, int NumeroLlegada, int Prioridad)
+        {
+            this.Id = Id;
+            this.LlegadaHospital = LlegadaHospital;
+            this.TiempoConsulta = TiempoConsulta;
+            this.Estado = 0;
+            this.NumeroLlegada = NumeroLlegada;
+            this.Prioridad = Prioridad;
         }
     }
 }
--- Ejercicio1/Tarea2/obj/Debug/net8.0/Tarea2.AssemblyInfo.cs
+++ Ejercicio1/Tarea2/obj/Debug/net8.0/Tarea2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Tarea1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8303cc13671c10717a8a2c38858a4eeb49818e")]
-[assembly: System.Reflection.AssemblyProductAttribute("Tarea1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Tarea1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Tarea2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
